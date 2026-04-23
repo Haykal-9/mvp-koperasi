360664c6a0e9ff85331f86c89b2cae96746c9f6b
M Haykal Lazuardy <Haykal-9>
2026-04-23 12:33:03 +0700

    deploy

---
+++ koperasi-frontend/api/index.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"net/http"
+
+	"koperasi-frontend/internal/server"
+)
+
+var app *http.Handler
+
+func init() {
+	r := server.SetupApp()
+	h := http.Handler(r)
+	app = &h
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	(*app).ServeHTTP(w, r)
+}
